508322e4ac2d8e8eb61beb182d4dd000c73e002e
li3tuo4 <li3tuo4@gmail.com>
2018-09-26 18:04:54 +1000

    remove a redundant tcl script

--- zcu102/src/tcl/make_bitstream_ZynqConfig.tcl
+++
@@ -1,8 +0,0 @@
-open_project zcu102_rocketchip_ZynqConfig/zcu102_rocketchip_ZynqConfig.xpr
-reset_run synth_1
-reset_run impl_1
-launch_runs synth_1
-wait_on_run synth_1
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1
-exit
