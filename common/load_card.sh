9d43366fdf7497b49bb22a45ae0938e22d051e66
li3tuo4 <li3tuo4@gmail.com>
2018-09-26 14:29:23 +1000

    update Makefile with compatible SD and ramfs scripts

--- common/load_card.sh
+++ common/load_card.sh
@@ -2,21 +2,27 @@
 
 if [ -z "$1" ]
   then
-    echo "Please provide a path to a SD card"
+    echo "Please provide the root path to a SD card"
     exit 1
 fi
 
-fpga_images_dir=`ls -d fpga-images-*`
+if [ ! -d "$1/BOOT" ]
+  then
+    echo "Cannot find the \"BOOT\" partition of SD card"
+    exit 1
+fi
+
+if [ ! -d "$1/rootfs" ]
+  then
+    echo "Cannot find the \"rootfs\" partition of SD card"
+    exit 1
+fi
+
+fpga_images_dir=fpga-images-$2
 
 set -x
-cp $fpga_images_dir/boot.bin $1
-cp $fpga_images_dir/devicetree.dtb $1
-cp $fpga_images_dir/uImage $1
-cp $fpga_images_dir/uramdisk.image.gz $1
-# if user downloaded riscv-linux, copy that over also
-if [ -a $fpga_images_dir/riscv/vmlinux ] 
-then 
-    mkdir -p $1/riscv
-    cp $fpga_images_dir/riscv/vmlinux $1/riscv/
-    cp $fpga_images_dir/riscv/root.bin $1/riscv/
-fi
+cp $fpga_images_dir/BOOT.BIN $1/BOOT/
+cp $fpga_images_dir/image.ub $1/BOOT/
+cd $fpga_images_dir/ && gzip -d initramfs.cpio.gz
+sudo cp initramfs.cpio $1/rootfs/
+cd $1/rootfs/; sudo pax -rf initramfs.cpio
