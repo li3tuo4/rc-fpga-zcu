1711917705e4e4a07a31db2ac20a40866ba8579e
Tuo Li <sam.of.ee@gmail.com>
2019-03-25 15:22:30 +1100

    fix from nick for sed path in petalinux_bd.sh

--- zcu102/soft_config/petalinux_bd.sh
+++ zcu102/soft_config/petalinux_bd.sh
@@ -16,7 +16,7 @@ cd $proj_dir
 #copy config file to the folder and replace the default config file
 cp -v $gen_config $proj_dir/project-spec/configs/
 #FIX the hardcoded path
-sed -i "s|FIXME_PLDIR|$proj_dir/g" $proj_dir/project-spec/configs/$gen_config
+sed -i "s@FIXME_PLDIR@$proj_dir@g" $proj_dir/project-spec/configs/config
 petalinux-config --get-hw-description=$config_dir --oldconfig
 #copy config file to the folder and replace the default config file
 cp -v $rootfs_config $proj_dir/project-spec/configs/
